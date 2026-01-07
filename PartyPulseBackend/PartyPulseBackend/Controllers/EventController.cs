1721c8f9f9f30d50ea95d2df661d87ccc56cf285
sandorr <sandorr@kkszki.hu>
2026-01-07 11:54:27 +0100

    Az összes esemény(Event) lekérdezéséhez szolgáló végpont elkészült

--- PartyPulseBackend/PartyPulseBackend/Controllers/EventController.cs
+++ PartyPulseBackend/PartyPulseBackend/Controllers/EventController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PartyPulseBackend.Data;
+using PartyPulseBackend.DTOs;
 
 namespace PartyPulseBackend.Controllers
 {
@@ -7,23 +10,31 @@ namespace PartyPulseBackend.Controllers
     [ApiController]
     public class EventController : ControllerBase
     {
-        private readonly PartyPulseContext_context;
+        private readonly PartyPulseContext _context;
 
         public EventController(PartyPulseContext context)
         {
             _context = context;
         }
         [HttpGet("AllEvents")]
-        public IActionResult GetAllEvents()
+        public async Task<IActionResult> GetAllEvents()
         {
             try
             {
-
+                var events =await _context.Events.Select(e=>new EventDTO
+                {
+                    Title = e.Title,
+                    Description = e.Description,
+                    EventDateTime = e.EventDateTime,
+                    Address = e.Address,
+                    LocationName = e.LocationName
+                })
+                    .ToListAsync();
+                return Ok(events);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
+                return BadRequest($"Hiba az események betöltésekor: {ex.Message}");
             }
         }
     }
