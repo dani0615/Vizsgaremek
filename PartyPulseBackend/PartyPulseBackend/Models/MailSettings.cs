8e2ed86022c7cb9c4525166e6f116ff58a58df4b
haluskad <haluskad@201-14>
2026-01-07 10:59:24 +0100

    MailSettingsDTO regisztrációhoz.

---
+++ PartyPulseBackend/PartyPulseBackend/Models/MailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace PartyPulseBackend.Models
+{
+    public class MailSettings
+    {
+        public string SmtpServer { get; set; }
+        public string SenderEmail { get; set; }
+        public string SenderName { get; set; }
+        public string SenderPassword { get; set; }
+        public int Port { get; set; }
+    }
+}
