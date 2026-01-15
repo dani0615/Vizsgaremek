7e014ec6adff68a1155e0b01bb45e8c1d024347b
Roland <Roland@WINDOWS-N88E8HR>
2026-01-15 09:48:55 +0100

    RankingController előkészítés a ranglista megjelenítéséhez

---
+++ PartyPulseBackend/PartyPulseBackend/Controllers/RankingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PartyPulseBackend.Data;
+
+namespace PartyPulseBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankingController : ControllerBase
+    {
+        private readonly PartyPulseContext _context;
+
+        public RankingController(PartyPulseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("TopList")]
+    }
+}
