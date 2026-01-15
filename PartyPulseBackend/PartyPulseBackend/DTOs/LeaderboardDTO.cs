5285326e2a1a47dcce33d28f05833555b951f8ac
Roland <Roland@WINDOWS-N88E8HR>
2026-01-15 09:45:57 +0100

    LeaderboardDTO elkészült

---
+++ PartyPulseBackend/PartyPulseBackend/DTOs/LeaderboardDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace PartyPulseBackend.DTOs
+{
+    public class LeaderboardDTO
+    {
+        public int Rank { get; set; }
+        public string UserName { get; set; }
+        public int PartyCount { get; set; }
+        public int Score { get; set; }
+    }
+}
