38775775e50c4bf65c609d9e8f2307ad88ca56e0
haluskad <haluskad@201-14>
2026-01-21 09:09:29 +0100

    Authorization controller , JWT alapú token generálás sikeres bejelentkezéskor

---
+++ PartyPulseBackend/PartyPulseBackend/Controllers/AuthController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using PartyPulseBackend.Data;
+using PartyPulseBackend.DTOs;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PartyPulseBackend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly PartyPulseContext _context;
+        private readonly IConfiguration _config;
+
+        public AuthController(PartyPulseContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDTO loginModel)
+        {
+           
+            var user = await _context.Users
+                .Include(u => u.Passwordsalt)
+                .FirstOrDefaultAsync(u => u.Email == loginModel.Email);
+
+            if (user == null)
+                return Unauthorized("Hibás email cím vagy jelszó.");
+
+           
+            if (user.IsActive != true)
+                return BadRequest("Kérjük, előbb erősítse meg az email címét!");
+
+          
+            string salt = user.Passwordsalt!.Salt;
+            string computedHash = Program.CreateSHA256(loginModel.Password + salt);
+
+            if (computedHash != user.Passwordsalt.PasswordHash)
+                return Unauthorized("Hibás email cím vagy jelszó.");
+
+          
+            var token = GenerateJwtToken(user);
+
+            return Ok(new
+            {
+                token = token,
+                username = user.Username,
+                role = user.Role
+            });
+        }
+
+        private string GenerateJwtToken(Models.User user)
+        {
+            var jwtSettings = _config.GetSection("JwtSettings");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["ExpiryMinutes"])),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
