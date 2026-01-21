878bd7cee816933e53347bcacf7b6cf4da59c892
sandorr <sandorr@kkszki.hu>
2026-01-21 09:27:34 +0100

    UpdateProfileDTO osztály elkészült

---
+++ PartyPulseBackend/PartyPulseBackend/DTOs/UpdateProfileDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PartyPulseBackend.DTOs
+{
+    public class UpdateProfileDTO
+    {
+        public string Email { get; set; } = null!;
+        public string? DisplayName { get; set; }
+        public string? Bio { get; set; }
+    }
+}
