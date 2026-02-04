9d7561f879bab80890cc696ed7ff025e5cd9d11f
sandorr <sandorr@kkszki.hu>
2026-02-04 10:12:05 +0100

    EventDTO újabb property-kel bővült

--- PartyPulseBackend/PartyPulseBackend/DTOs/EventDTO.cs
+++ PartyPulseBackend/PartyPulseBackend/DTOs/EventDTO.cs
@@ -2,11 +2,15 @@
 {
     public class EventDTO
     {
+        public int EventID { get; set; }
         public string Title { get; set; }
         public string?  Description { get; set; }
         public DateTime EventDateTime { get; set; }
         public string? Address { get; set; }
         public string? LocationName { get; set; }
         public string? ImageUrl { get; set; }
+        public string? MusicStyle { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
     }
 }
