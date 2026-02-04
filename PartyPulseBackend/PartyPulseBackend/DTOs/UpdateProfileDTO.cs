ac37f765a2a9047140b1c8038f492b68621a8d3a
sandorr <sandorr@kkszki.hu>
2026-02-04 10:14:30 +0100

    UpdateProfileDTO új propertyk felvétele

--- PartyPulseBackend/PartyPulseBackend/DTOs/UpdateProfileDTO.cs
+++ PartyPulseBackend/PartyPulseBackend/DTOs/UpdateProfileDTO.cs
@@ -5,5 +5,8 @@
         public string Email { get; set; } = null!;
         public string? DisplayName { get; set; }
         public string? Bio { get; set; }
+        public string? Gender { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? LookingFor { get; set; }
     }
 }
