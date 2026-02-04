057c5ce85a36ed21ff03b6f173303994f4ae23ee
sandorr <sandorr@kkszki.hu>
2026-02-04 10:19:21 +0100

    UserRegistrationDTO-ba új propertyk felvétele

--- PartyPulseBackend/PartyPulseBackend/DTOs/UserRegistrationDTO.cs
+++ PartyPulseBackend/PartyPulseBackend/DTOs/UserRegistrationDTO.cs
@@ -5,5 +5,9 @@
         public string Username { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
+        public string? Gender { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? LookingFor { get; set; }
+        public string? DisplayName { get; set; }
     }
 }
