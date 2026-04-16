90a5eea62085a111ed26e6c0591459892fda5470
Roland <Roland@WINDOWS-N88E8HR>
2026-04-16 10:12:23 +0200

    ResetPasswordDTO hozzáadva a DTO mappához

---
+++ PartyPulseBackend/PartyPulseBackend/DTOs/ResetPasswordDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PartyPulseBackend.DTOs
+{
+    public class ResetPasswordDTO
+    {
+        public string Email { get; set; } = null!;
+        public string Token { get; set; } = null!;
+        public string NewPassword { get; set; } = null!;
+    }
+}
