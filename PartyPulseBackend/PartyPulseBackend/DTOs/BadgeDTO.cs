9f6fc54db9f5c39bf0e4fc1ca44e81f3c5cc6b97
Roland <Roland@WINDOWS-N88E8HR>
2026-04-16 10:13:27 +0200

    BadgeDTO kész

---
+++ PartyPulseBackend/PartyPulseBackend/DTOs/BadgeDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace PartyPulseBackend.DTOs
+{
+    public class BadgeDTO
+    {
+        public int BadgeID { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? IconUrl { get; set; }
+        public string Criteria { get; set; } = null!;
+        public bool IsEarned { get; set; }
+        public DateTime? AwardedAt { get; set; }
+        public bool IsPinned { get; set; }
+    }
+}
